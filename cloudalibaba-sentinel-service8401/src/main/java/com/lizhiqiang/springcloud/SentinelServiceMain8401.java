06f8f69a60f9f042233ca4a2e12636940c60bf52
lizhiqiang <lizhiqiang@cnhis.com>
2020-09-29 15:45:32 +0800

    sentinel 将限流配置规则持久化进Nacos 配置中心保存

--- cloudalibaba-sentinel-service8401/src/main/java/com/lizhiqiang/springcloud/SentinelServiceMain8401.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/lizhiqiang/springcloud/SentinelServiceMain8401.java
@@ -2,10 +2,9 @@ package com.lizhiqiang.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient //yml配置了连接到nacos注册中心，相当于开启了
 public class SentinelServiceMain8401 {
     public static void main(String[] args) {
         SpringApplication.run(SentinelServiceMain8401.class, args);
